f70ed3e0661a5d03d31801811a29c84cb01bb2b6
Xen24 <Xen24@outlook.de>
2025-03-12 18:12:20 +0100

    added proxalert force op update

--- src/main/java/io/github/spigotrce/paradiseclientfabric/packet/ProxAlertPayloadPacket.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/packet/ProxAlertPayloadPacket.java
@@ -1,2 +1,42 @@
-package io.github.spigotrce.paradiseclientfabric.packet;public class ProxAlertPayloadPacket {
+package io.github.spigotrce.paradiseclientfabric.packet;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+public record ProxAlertPayloadPacket(String s) implements CustomPayload {
+    public static final PacketCodec<PacketByteBuf, ProxAlertPayloadPacket> CODEC = CustomPayload.codecOf(ProxAlertPayloadPacket::write, ProxAlertPayloadPacket::new);
+    public static final CustomPayload.Id<ProxAlertPayloadPacket> ID = new CustomPayload.Id<>(Identifier.of("proxalert", "alert"));
+
+    private ProxAlertPayloadPacket(PacketByteBuf buf) {
+        this(buf.readString());
+    }
+
+    private void write(PacketByteBuf buf) {
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("BACKEND_CONSOLE_COMMAND");
+        out.writeUTF("op");
+        out.writeUTF(s);
+    }
+
+    public CustomPayload.Id<ProxAlertPayloadPacket> getId() {
+        return ID;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "psab-spammer";
+    }
 }
